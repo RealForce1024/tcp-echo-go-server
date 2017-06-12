8e78d2db1a9bf58e4a7baae7c57ef225aac12ccd
fqc <337940626@qq.com>
2017-06-12 22:13:43 +0800

    udpate

---
+++ test/server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net"
+	"bufio"
+	"io"
+	"fmt"
+	"os"
+)
+
+func echo(conn net.Conn) {
+	r := bufio.NewReader(conn)
+	for {
+		line, err := r.ReadBytes(byte('\n'))
+		switch err {
+		case nil:
+			break
+		case io.EOF:
+		default:
+			fmt.Println("eror", err)
+		}
+		conn.Write(line)
+	}
+}
+
+var server = "localhost" // if localhost this can be ignore
+var port = ":3545"  // ignore local ip,should add ":"and port
+
+func main() {
+	l, err := net.Listen("tcp", port)
+
+	if err != nil {
+		fmt.Println("error", err)
+		os.Exit(1)
+	}
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("err:", err)
+			continue
+		}
+		go echo(conn)
+	}
+
+}
